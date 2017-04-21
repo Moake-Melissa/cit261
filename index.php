5c2a2a95c8e55d1ba515c0b5f23c0ac74b8c5674
Moake-Melissa <moa13007@byui.edu>
2017-04-21 17:49:35 -0600

    index page added for my remote site

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>CIT 261 Home Page</title>
+	</head>
+	<body>
+		<h1>Welcome to CIT 261!</h1>
+                <p>This is where I will prove what I learn in the class.</p>
+	</body>
+</html>
