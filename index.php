1ce4ab2ef84277858bf32ab3671036ee81adf9a7
Moake-Melissa <moa13007@byui.edu>
2017-05-11 20:46:31 -0600

    dom example complete with add, edit, and delete functionality

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 	<body id="home">
 		<h1>Welcome to my CIT 261 Portfolio!</h1>
                 <p>This is where I will prove what I learn in the class.</p>
-                <br>
                 <table>
                     <thead>
                         <tr>
@@ -23,15 +22,15 @@
                         <tr><td><b>AJAX</b> to Consume a JSON Web Service
 </td><td>Not Submitted</td><td></td></tr>
                         <tr><td><a href="advancedLocalStorage.html"><b>Local Storage</b> API, Storing and Retrieving Simple Data, Arrays, Associative Arrays, and Objects</a></td><td>Not Submitted</td><td></td></tr>
-                        <tr><td><b>DOM</b> Manipulation Using createElement, appendChild, insertBefore, removeChild, etc.</td><td>Not Submitted</td><td></td></tr>
+                        <tr><td><a href="dom.html"><b>DOM</b> Manipulation Using createElement, appendChild, insertBefore, removeChild, etc.</a></td><td>Not Submitted</td><td></td></tr>
                         <tr><td>Manipulating <b>CSS</b> Class Properties <b>Using JavaScript</td><td>Not Submitted</td><td></td></tr>
                         <tr><td>Creating <b>CSS3</b> Transitions and Animations in CSS and <b>triggering</b> them with <b>JavaScript</b></td><td>Not Submitted</td><td></td></tr>
                         <tr><td><b>JavaScript Events</b> Including those for Mobile Devices ( Ex. onTouchBegin, onLoad, etc.) and Animation and Transition Events
 </td><td>Not Submitted</td><td></td></tr>
-                        <tr><td><a href="html5Features.html"><b>HTML5 Tags</b> - Video, Audio, and Canvas</a></td><td><b>Ready for Review</b></td><td></td></tr>
-                        <tr><td>11.Designing, Defining, and Triggering CSS3 <b>Transitions</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
-                        <tr><td>12.Designing, Defining, and Triggering CSS3 <b>Transforms</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
-                        <tr><td>13.Designing, Defining, and Triggering CSS3 <b>Animations</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
+                        <tr><td><a href="html5Features.html"><b>HTML5 Tags</b> - Video, Audio, and Canvas</a></td><td><b>Complete</b><br>Grade: Skilled</td><td>Submitted May 6. Feedback given May 7.</td></tr>
+                        <tr><td>Designing, Defining, and Triggering CSS3 <b>Transitions</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
+                        <tr><td>Designing, Defining, and Triggering CSS3 <b>Transforms</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
+                        <tr><td>Designing, Defining, and Triggering CSS3 <b>Animations</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
                     </tbody>
                 </table>
 	</body>
