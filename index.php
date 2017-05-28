a5a4b381c2508928dd4ed307dc3976b003809482
Moake-Melissa <moa13007@byui.edu>
2017-05-27 22:29:05 -0600

    css animation example for presentation and beginnings of css trio game

--- index.php
+++ index.php
@@ -16,21 +16,21 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr><td><b>JavaScript</b> Loops, Conditional Statements, Functions, Variables, Parameters, Arrays, Associative Arrays</td><td>Not Submitted</td><td>My DOM example uses some of these features (variables, functions, conditionals).</td></tr>
-                        <tr><td><b>JavaScript</b> - Object Creation, Functions, Properties, Methods</td><td>Graded: Basic</td><td>Combine with other topics for higher grade.</td></tr>
-                        <tr><td><a href='dom-json.html'><b>JSON</b> Parse, Stringify</a></td><td>Ready to Review</td><td>Submitted May 19: This covers DOM, Local Storage, JSON, and AJAX</td></tr>
+                        <tr><td><b>JavaScript</b> Loops, Conditional Statements, Functions, Variables, Parameters, Arrays, Associative Arrays</td><td><b>Complete</b><br>Grade: Skilled</td><td>My DOM example uses some of these features (variables, functions, conditionals). The JSON link uses the rest. <br> May 12th: Working. May 19th: Skilled.</td></tr>
+                        <tr><td><b>JavaScript</b> - Object Creation, Functions, Properties, Methods</td><td><b>Complete</b><br>Grade: Skilled</td></td><td>My DOM example uses some of these features (methods). The JSON link uses the rest. <br> May 12th: Working. May 19th: Skilled.</td></tr>
+                        <tr><td><a href='dom-json.html'><b>JSON</b> Parse, Stringify</a></td><td><b>Complete</b><br>Grade: Skilled</td></td><td>This covers DOM, Local Storage, JSON, and AJAX <br> Submitted May 19th Graded May 19th. </td></tr>
                         <tr><td><b>AJAX</b> to Consume a JSON Web Service
-</td><td>Not Submitted</td><td></td></tr>
-                        <tr><td><a href="advancedLocalStorage.html"><b>Local Storage</b> API, Storing and Retrieving Simple Data, Arrays, Associative Arrays, and Objects</a></td><td>Not Submitted</td><td>See JSON Link for example</td></tr>
-                        <tr><td><a href="dom.html"><b>DOM</b> Manipulation Using createElement, appendChild, insertBefore, removeChild, etc.</a></td><td>Complete<br>Graded: Working</td><td>Ready for review on May 12th.</td></tr>
-                        <tr><td>Manipulating <b>CSS</b> Class Properties <b>Using JavaScript</td><td>Graded: Working</td><td>Need to combine with other topic for skilled grade.</td></tr>
+</td><td><b>Complete</b><br>Grade: Skilled</td></td><td>See JSON link for example. <br> Submitted May 19th Graded May 19th.</td></tr>
+                        <tr><td><a href="localStorage.html"><b>Local Storage</b> API, Storing and Retrieving Simple Data, Arrays, Associative Arrays, and Objects</a></td><td><b>Complete</b><br>Grade: Skilled</td></td><td>See JSON Link for example</td></tr>
+                        <tr><td><a href="dom.html"><b>DOM</b> Manipulation Using createElement, appendChild, insertBefore, removeChild, etc.</a></td><td><b>Complete</b><br>Grade: Skilled</td><td>Ready for review on May 12th. May 12: Grade Working. May 19th Grade Skilled (see JSON link for Skilled Example)</td></tr>
+                <tr><td>Manipulating <b>CSS</b> Class Properties <b>Using JavaScript</td><td>Graded: Working</td><td>Need to combine with other topic for skilled grade. <br> Graded Skilled on May 12th</td></tr>
                         <tr><td>Creating <b>CSS3</b> Transitions and Animations in CSS and <b>triggering</b> them with <b>JavaScript</b></td><td>Not Submitted</td><td></td></tr>
                         <tr><td><b>JavaScript Events</b> Including those for Mobile Devices ( Ex. onTouchBegin, onLoad, etc.) and Animation and Transition Events
 </td><td>Not Submitted</td><td></td></tr>
-                        <tr><td><a href="html5Features.html"><b>HTML5 Tags</b> - Video, Audio, and Canvas</a></td><td><b>Complete</b><br>Grade: Skilled</td><td>Submitted May 6. Feedback given May 7.</td></tr>
+                        <tr><td><a href="html5Features.html"><b>HTML5 Tags</b> - Video, Audio, and Canvas</a></td><td><b>Complete</b><br>Grade: Skilled</td><td>Submitted May 6. Grade Skilled on May 7.</td></tr>
                         <tr><td>Designing, Defining, and Triggering CSS3 <b>Transitions</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
                         <tr><td>Designing, Defining, and Triggering CSS3 <b>Transforms</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
-                        <tr><td>Designing, Defining, and Triggering CSS3 <b>Animations</b> without Custom Libraries</td><td>Not Submitted</td><td></td></tr>
+                        <tr><td><a href="cssAnimations.html">Designing, Defining, and Triggering CSS3 <b>Animations</b> without Custom Libraries</a></td><td>Not Submitted</td><td></td></tr>
                     </tbody>
                 </table>
 	</body>
